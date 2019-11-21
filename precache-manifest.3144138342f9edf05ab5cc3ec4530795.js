670f7421cc24e8de0977cec496f81d28e798f0ed
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2019-11-21 09:53:50 +0300

    Updates

---
+++ precache-manifest.3144138342f9edf05ab5cc3ec4530795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d873c00466d6e56a4ec2b69c4228d3",
+    "url": "/Gaydamovich/test-project/folder/index.html"
+  },
+  {
+    "revision": "74ad6d887235796e9f48",
+    "url": "/Gaydamovich/test-project/folder/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8078d47930c815e154e4",
+    "url": "/Gaydamovich/test-project/folder/static/css/main.511acf54.chunk.css"
+  },
+  {
+    "revision": "74ad6d887235796e9f48",
+    "url": "/Gaydamovich/test-project/folder/static/js/2.f27d4a19.chunk.js"
+  },
+  {
+    "revision": "8078d47930c815e154e4",
+    "url": "/Gaydamovich/test-project/folder/static/js/main.f70c41d7.chunk.js"
+  },
+  {
+    "revision": "44031c7f820a11b58686",
+    "url": "/Gaydamovich/test-project/folder/static/js/runtime~main.ef0f07bf.js"
+  }
+]);
